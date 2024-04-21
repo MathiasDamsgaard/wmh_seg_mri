b3dff98b3a09a8fd1269c6b91723b3b8cea39320
MathiasDamsgaard <mathias.c.damsgaard@gmail.com>
2024-04-21 10:09:42 +0200

    Update to the evaluation files and creating of a new environment for that

--- evaluate_wmh.py
+++ evaluate_wmh.py
@@ -1,9 +1,11 @@
 import nibabel as nib
 import numpy as np
 import os
-# from skimage.metrics import hausdorff_distance
+from tqdm import tqdm
+from skimage.metrics import hausdorff_distance as cal_hd
+from medpy.metric.binary import hd95
 
-def dice_score(datak1, data2):
+def cal_dice(data1, data2):
     # Calculate Dice score
     flat_data1 = data1.flatten()
     flat_data2 = data2.flatten()
@@ -16,37 +18,61 @@ def dice_score(datak1, data2):
     return dice
 
 # get data
-path_gt = '/home/rosshaug/DATASET/nnFormer_raw/nnFormer_raw_data/Task001_wmh/labelsTs'
-path_infer = '/home/rosshaug/DATASET/nnFormer_raw/nnFormer_raw_data/Task001_wmh/inferTs/nnformer_wmh'
+task_path = '/mnt/projects/whmseg/nobackup/DATASET/nnFormer_raw/nnFormer_raw_data/Task001_wmh/'
+gt_path = task_path + 'labelsTs'
+infer_path = task_path + 'inferTs/nnformer_wmh_nds'
 
-gt_data = sorted([f for f in os.listdir(path_gt) if os.path.isfile(os.path.join(path_gt,f)) and f[-6:]=="nii.gz"])
-infer_data = sorted([f for f in os.listdir(path_infer) if os.path.isfile(os.path.join(path_gt,f)) and f[-6:]=="nii.gz"])
-print(infer_data)
+label_data = sorted([f for f in os.listdir(gt_path) if os.path.isfile(os.path.join(gt_path,f)) and f[-6:]=="nii.gz"])
+infer_data = sorted([f for f in os.listdir(infer_path) if os.path.isfile(os.path.join(infer_path,f)) and f[-6:]=="nii.gz"])
 
 # check if infered labels and test labels match
-if gt_data == infer_data:
-    pass
+if label_data == infer_data:
+    print("Data loaded")
 else:
     print("Groundtruth data and infered data does not match")
 
+# create file to write results to
+fw = open(infer_path + '/evalution.txt', 'w')
 
 # calculate metrics
 dice_scores = []
 hausdorff_distances = []
+hd95_score = []
 
-for f in infer_data:
+print("Calculating...")
+for f in tqdm(infer_data):
     # Load data and convert to numpy
-    img1 = nib.load(os.path.join(path_gt,f))
-    img2 = nib.load(os.path.join(path_infer,f))
+    gt_img = nib.load(os.path.join(gt_path,f))
+    infer_img = nib.load(os.path.join(infer_path,f))
     
-    data1 = np.asarray(img1.dataobj)
-    data2 = np.asarray(img2.dataobj)
+    gt = np.asarray(gt_img.dataobj)
+    infer = np.asarray(infer_img.dataobj)
     
     # metrics
-    dice_scores.append(dice_score(data1,data2))
-    #hausdorff_distances()
+    dice_scores.append(cal_dice(gt, infer))
+    hausdorff_distances.append(cal_hd(gt, infer))
+    hd95_score.append(hd95(gt, infer))
 
+    # write metrics to the file
+    fw.write('*'*20+'\n',)
+    fw.write(infer_path.split('/')[-1]+'\n')
+    fw.write('HD: {:.4f}\n'.format(hausdorff_distances[-1]))
+    fw.write('Dice: {:.4f}\n'.format(dice_scores[-1]))
 
+# calculate average scores
+avg_dice = np.mean(dice_scores)
+avg_hd = np.mean(hausdorff_distances)
 
-print(dice_scores)
-print(np.mean(dice_scores))
+print("Dice scores:")
+#print(dice_scores)
+print(avg_dice)
+print("Hausdorff distances:")
+#print(hausdorff_distances)
+print(avg_hd)
+
+print("HD95: ", np.mean(hd95_score))
+
+fw.write('*'*20+'\n',)
+fw.write('Dice: '+str(avg_dice)+' '+'\n')
+fw.write('HD: '+str(avg_hd)+' '+'\n')
+fw.close()
--- nnformer/inference_wmh.py
+++ nnformer/inference_wmh.py
@@ -28,26 +28,14 @@ def hd(pred,gt):
         return 0
         
 def process_label(label):
-    net = label == 2
-    ed = label == 1
-    et = label == 3
-    ET=et
-    TC=net+et
-    WT=net+et+ed
-    return ET,TC,WT
+    return label == 1
 
 def test(fold):
-    path='./'
+    path='./../nobackup/DATASET/nnFormer_raw/nnFormer_raw_data/Task001_wmh/'
     label_list=sorted(glob.glob(os.path.join(path,'labelsTs','*nii.gz')))
     infer_list=sorted(glob.glob(os.path.join(path,'inferTs',fold,'*nii.gz')))
     print("loading success...")
-    Dice_et=[]
-    Dice_tc=[]
-    Dice_wt=[]
-    
-    HD_et=[]
-    HD_tc=[]
-    HD_wt=[]
+    Dice, HD = [], []
     file=path + 'inferTs/'+fold
     fw = open(file+'/dice_pre.txt', 'w')
 
@@ -55,53 +43,27 @@ def test(fold):
         print(label_path.split('/')[-1])
         print(infer_path.split('/')[-1])
         label,infer = read_nii(label_path),read_nii(infer_path)
-        label_et,label_tc,label_wt=process_label(label)
-        infer_et,infer_tc,infer_wt=process_label(infer)
-        Dice_et.append(dice(infer_et,label_et))
-        Dice_tc.append(dice(infer_tc,label_tc))
-        Dice_wt.append(dice(infer_wt,label_wt))
+        label_p = process_label(label)
+        infer_p = process_label(infer)
+        Dice.append(dice(infer_p,label_p))
         
-        HD_et.append(hd(infer_et,label_et))
-        HD_tc.append(hd(infer_tc,label_tc))
-        HD_wt.append(hd(infer_wt,label_wt))
+        HD.append(hd(infer_p,label_p))
         
         fw.write('*'*20+'\n',)
         fw.write(infer_path.split('/')[-1]+'\n')
-        fw.write('hd_et: {:.4f}\n'.format(HD_et[-1]))
-        fw.write('hd_tc: {:.4f}\n'.format(HD_tc[-1]))
-        fw.write('hd_wt: {:.4f}\n'.format(HD_wt[-1]))
-        fw.write('*'*20+'\n',)
-        fw.write('Dice_et: {:.4f}\n'.format(Dice_et[-1]))
-        fw.write('Dice_tc: {:.4f}\n'.format(Dice_tc[-1]))
-        fw.write('Dice_wt: {:.4f}\n'.format(Dice_wt[-1]))
+        fw.write('hd: {:.4f}\n'.format(HD[-1]))
+        fw.write('Dice: {:.4f}\n'.format(Dice[-1]))
+        #print('dice: {:.4f}'.format(np.mean(Dice)))
 
-        #print('dice_et: {:.4f}'.format(np.mean(Dice_et)))
-        #print('dice_tc: {:.4f}'.format(np.mean(Dice_tc)))
-        #print('dice_wt: {:.4f}'.format(np.mean(Dice_wt)))
-    dsc=[]
-    avg_hd=[]
-    dsc.append(np.mean(Dice_et))
-    dsc.append(np.mean(Dice_tc))
-    dsc.append(np.mean(Dice_wt))
+    avg_dsc = np.mean(Dice)
+    avg_hd = np.mean(HD)
+    fw.write('*'*20+'\n',)
+    fw.write('Dice: '+str(avg_dsc)+' '+'\n')
+    fw.write('HD: '+str(avg_hd)+' '+'\n')
+    fw.close()
     
-    avg_hd.append(np.mean(HD_et))
-    avg_hd.append(np.mean(HD_tc))
-    avg_hd.append(np.mean(HD_wt))
-    
-    fw.write('Dice_et'+str(np.mean(Dice_et))+' '+'\n')
-    fw.write('Dice_tc'+str(np.mean(Dice_tc))+' '+'\n')
-    fw.write('Dice_wt'+str(np.mean(Dice_wt))+' '+'\n')
-    
-    fw.write('HD_et'+str(np.mean(HD_et))+' '+'\n')
-    fw.write('HD_tc'+str(np.mean(HD_tc))+' '+'\n')
-    fw.write('HD_wt'+str(np.mean(HD_wt))+' '+'\n')
-    
-    fw.write('Dice'+str(np.mean(dsc))+' '+'\n')
-    fw.write('HD'+str(np.mean(avg_hd))+' '+'\n')
-    #print('Dice'+str(np.mean(dsc))+' '+'\n')
-    #print('HD'+str(np.mean(avg_hd))+' '+'\n')
-    
-
+    #print('Dice'+str(avg_dsc)+' '+'\n')
+    #print('HD'+str(avg_hd)+' '+'\n')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
